e4634e7925b019bc2a9bebeb9181eef002f9a3e6
leonardo farinha <leonardofarinhap@outlook.com>
2017-06-24 15:08:07 -0500

    added feature to read from csv and store it in array

---
+++ src/sample/TestMain.java
@@ -0,0 +1,19 @@
+package sample;
+
+/**
+ * Created by lfarinha on 6/24/17.
+ */
+public class TestMain
+{
+    public static void main(String[] args)
+    {
+       Mysql sql = new Mysql();
+       Import csv = new Import("src/sample/names.csv");
+        for(int i=0; i<100; i++) {
+            for (int j=0; j <=4; j++) {
+                System.out.println(csv.returnCustomerData()[i][j]);
+            }
+        }
+//       sql.insertRowsInCustomer();
+    }
+}
